285c88824246a268ceb7cb417fa7493032675873
PaulSchmidmeister <gym.paul.schmidmeister@pindl-schueler.de>
2026-06-25 15:02:02 +0200

    r

--- Assets/Scripts/hud.gd
+++ Assets/Scripts/hud.gd
@@ -3,6 +3,7 @@ class_name HUD
 
 @export var münze_label : Label
 @export var tür_label : Label
+@export var tod_label : Label
 
 func update_münze_label(number : int):
 	münze_label.text = "X " + str(number)
@@ -12,3 +13,6 @@ func tür_opened():
 
 func tür_closed():
 	tür_label.text = "tür geschlossen ... hole Münzen! "
+
+func update_tod_label(number : int) :
+	tod_label.text = "Tod" + str(number)
