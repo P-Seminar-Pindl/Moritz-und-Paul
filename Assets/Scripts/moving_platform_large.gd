609178aa67a1acd35670dc5564b32c7a97889e73
PaulSchmidmeister <gym.paul.schmidmeister@pindl-schueler.de>
2026-07-02 15:11:46 +0200

    k

--- Assets/Scripts/moving_platform_large.gd
+++ Assets/Scripts/moving_platform_large.gd
@@ -1,7 +1,7 @@
 extends Path2D
 class_name MovingPlatform 
 
-@export var path_time = 0.5
+@export var path_time = 0.2
 @export var looping = false
 @export var ease : Tween.EaseType
 @export var transition : Tween.TransitionType
